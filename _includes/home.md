7ddcc35b43978e04d983f9c3d3ef40f8eb5cf25f
Hamza Essaoui <hamza@essaoui.net>
2013-09-20 18:18:56 -0700

    First Commit

---
+++ _includes/home.md
@@ -0,0 +1,26 @@
+Okta API Documentation
+======================
+
+This is the starting point for documentation on the Okta API. If you are new, take a look at the getting started section below.
+
+Contents
+--------
+
+### Getting Started
+* [Getting a token](getting_started/getting_a_token.md)
+* [Design principles](getting_started/design_principles.md)
+* [Error Codes](getting_started/error_codes.md)
+
+### Endpoints
+* [Users](endpoints/users.md)
+* [Groups](endpoints/groups.md)
+* [Sessions](endpoints/sessions.md)
+* [Tokens](endpoints/tokens.md)
+* [Events](endpoints/events.md)
+
+### Examples
+* [Retrieving a session cookie with a hidden image](examples/session_cookie.md)
+
+### Okta Identity Management
+Workday-Driven Identity Lifecycle Management with Okta Identity Management
+Okta automates user management into all leading cloud and web applications, and this process typically starts from a corporate directory like Active Directory. Now, with Workday-driven identity lifecycle management those provisioning and deprovisioning processes can be driven automatically from Workday via Okta identity management. And unlike integrations between HCM and Identity Management systems in this past, this integration is productized, robust, and cost effective to implement and maintain over time.
---
+++ assets/js/function.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+        $(function() { 
+            var counter = 0
+          $.ajax({
+            url:'/st-feed.xml',
+            dataType:'xml',
+            type:'GET',
+            success:function(xml) {
+              $(xml).find('entry').slice(0,  $('#forum-question').attr('data-limit')).each(function( counter ) {
+                var title = $(this).find("title").text(); 
+                var des = $(this).find("description").text();
+                var updated = $(this).find("updated").text();
+                var link = $(this).find("link").text();
+                var href = $(this).find("id").text();
+                var $link = $("<a class='st-link'></a>").attr('href',href).attr('target','_blank').html(title);
+                var date = new Date(updated);
+                var $date = $('<p class="date"></p>').text(ISODateString(date)   );
+                var wrapper = "<li class='single-feed'>";
+                $(".stackoverflow-feed").append($(wrapper).append($link,$date));                   })
+            },
+            error:function() {
+              console.log("I am sorry, But I can't fetch that feed");
+            }
+          });
+        });
+
+         $(function() {
+           var toc = $("#sidebar").tocify({ 
+            highlightDefault: true,
+            showAndHide: true,
+            scrollTo: 80,
+            selector: "h1,h2" });
+        });
+
+        function ISODateString(d){
+          function pad(n){return n<10 ? '0'+n : n}
+          return d.getUTCMonth()+'-'
+              + pad(d.getUTCMonth()+1)+'-'
+              + pad(d.getUTCFullYear())+'  '
+              + pad(d.getUTCHours())+':'
+              + pad(d.getUTCMinutes())+':'
+              + pad(d.getUTCSeconds());
+        }
+});
